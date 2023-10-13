4d82fc37a1087088879b8842e15c064a8991944b
Julian Rogawski <julianrogawski@gmail.com>
2023-10-13 17:20:16 +0200

    Update sample in start_tool_vr.py
    Update sample downloads in start_tool_vr.py

--- start_tool_vr.py
+++ start_tool_vr.py
@@ -26,11 +26,11 @@ RENDER_MODES: List[Tuple[int, int]] = [(3, 2), (4, 1), (1, 1), (2, 2)]
 def download_and_unzip_sample() -> str:
     output_directory = DATA_PATH
     filename = wget.download(
-        'https://drive.google.com/uc?export=download&id=1LiVzBfB7LPrR95q_VO44wx4MyGNTj6vD', out=output_directory)
+        'https://drive.google.com/uc?export=download&id=1EpsubJhHH4shqzDhsBB0SHsBjWgWa03S', out=output_directory)
     zip_filepath = os.path.join(output_directory, filename)
     with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
         zip_ref.extractall(DATA_PATH)
-    return os.path.join(DATA_PATH, 'sample_model.npz')
+    return os.path.join(DATA_PATH, 'sample_model.pro.npz')
 
 
 def open_processed_network(option_gui: OptionGui, filename: str) -> None:
